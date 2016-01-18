47e82627e61f0c75d86e669407e8113e598aac5d
William Stevens <contact@wastevensv.com>
2016-01-18 11:36:41 -0500

    Keys test prints GPIO status and keycode to LCD.

--- keys/main.c
+++ keys/main.c
@@ -1,9 +1,5 @@
-#include <stdlib.h>
-#include <avr/io.h>
 #include <util/delay.h>
-#include <avr/interrupt.h>
 #include "i2c.h"
-#include "macros.h"
 #include "lcd.h"
 #include "mcp23017.h"
 
@@ -20,34 +16,21 @@ const uint16_t keycodes[KEY_COUNT] =
 
 const uint8_t keyvals[KEY_COUNT] =
     { '/', '*', '-', '+',
-      '3', '6', '9', '=',
-      '2', '5', '8', '0',
-      '1', '4', '7', ' '
+      '=', ' ', '9', '8',
+      '7', '6', '5', '4',
+      '3', '2', '1', '0'
     };
 
 const uint8_t hexkey[16] = "0123456789ABCDEF";
-
-ISR(INT0_vect) {
-    PORTB &= ~(BV(PB5));
-    newkey=1;
-}
-
-void initInterrupt0(void) {
-    EIMSK |= (1 << INT0);
-    EICRA |= (1 << ISC01);
-}
 
 int main (void)
 {
     uint8_t input[16] = {'\0'};
     uint8_t input_len = 0;
     uint8_t i=0;
-    uint8_t key='F';
-    uint8_t pair=0;
+    uint8_t key='x';
     uint16_t keycode=0xFFFF;
 
-    DDRB |= BV(PB5);
-    PORTB |= BV(PB5);
     initI2C();
 
     mcp_write_register(MCP_REG_IOCON, 0x60);
@@ -56,21 +39,30 @@ int main (void)
 
     lcd_print("Hi!");
 
-    //initInterrupt0();
-    
     while (1) {
         keycode = 0x0000;
         keycode = ~mcp_read_ab();
         
+        _delay_ms(250);
+
         input_len = 0;
-        lcd_clear();
         input[input_len++] = hexkey[ (keycode & 0x000F)];
         input[input_len++] = hexkey[((keycode & 0x00F0) >> 4)];
         input[input_len++] = hexkey[((keycode & 0x0F00) >> 8)];
         input[input_len++] = hexkey[((keycode & 0xF000) >> 12)];
+
+        key = 'x';
+        for(i = 0; i < KEY_COUNT; i++) {
+            if(keycode == keycodes[i]) key = keyvals[i];
+        }
+
+        if(key != 'x') {
+            input[input_len++] = ' ';
+            input[input_len++] = key;
+        }
         input[input_len++] = '\0';
+
+        lcd_clear();
         lcd_print(input);
-        
-        _delay_ms(250);
     }
 }
