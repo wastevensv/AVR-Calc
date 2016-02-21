aef7888168b83010900115cdaaebcf80ce355f75
William Stevens <contact@wastevensv.com>
2016-02-21 16:32:30 -0500

    lang-pc demo can now accept input from stdin.

--- lang-pc/main.c
+++ lang-pc/main.c
@@ -5,7 +5,8 @@
 
 int main()
 {
-    uint8_t   input_str[] = "66 2 22+ -";
+    uint8_t   input_str[256];
+    fgets(input_str,255,stdin);
     uint8_t*  nextc = input_str;
     while(*nextc != 0x00) {
         int64_t a, b = 0;
